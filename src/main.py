54afd4f5d3f3a24720a5e468428b73e169c3b51e
Alina Baranova <alina_nemo@mail.ru>
2024-04-24 09:30:30 +0300

    Edit main file after all changes in functions file

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from functions import get_all_operation, get_update_operations_list, get_filtered_operation, get_sort_operation
+import os.path
+
+from functions import get_all_operation, get_update_operations_list, get_executed_operations, get_sort_operation
 
 
 def main():
-    operations_list = get_all_operation()
-    filtered_operation = get_filtered_operation(operations_list)
-    sorted_operation = get_sort_operation(filtered_operation, 5)
+    operations_list = get_all_operation('operation.json')
+    executed_operations = get_executed_operations(operations_list)
+    sorted_operation = get_sort_operation(executed_operations, 5)
     get_update_operations_list(sorted_operation)
     for operation in sorted_operation:
         print(f'''{operation['date']} {operation['description']}
